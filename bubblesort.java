0f9d508145fd9b9a543210c33377012e41ef3caf
SoniBrosSr7 <tarunethics5@gmail.com>
2023-02-02 15:02:02 +0530

    bubblesort added

---
+++ bubblesort.java
@@ -0,0 +1,33 @@
+public class bubblesort {
+
+	    void bubbleSorting(int arr[]){
+	        int n = arr.length;
+
+	        for (int i=0; i< n-1; i++){
+	            for (int j=0; j< n-i-1; j++){
+	                if (arr[j]>arr[j+1]){
+	                    int temp = arr[j];
+	                    arr[j] = arr[j+1];
+	                    arr[j+1]=temp;
+	                }
+	            }
+	        }
+	    }
+
+		void printArray(int arr[]){
+			int n = arr.length;
+			for (int i =0; i< n ; ++i){
+				System.out.println(arr[i]+ "");
+			}
+			System.out.println();
+		}
+		
+	public static void main (String args[]){
+	    bubblesort obj = new bubblesort();
+	    int arr[] = {5,1,8,7,9};
+	    obj.bubbleSorting(arr);
+	    obj.printArray(arr);
+	}
+
+}
+
